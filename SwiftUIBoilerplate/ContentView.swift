8cd8a2882be14ab9d2e6d0d3595d80f5100b1d13
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2020-11-19 12:11:18 +0900

    Initial Commit

---
+++ SwiftUIBoilerplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBoilerplate
+//
+//  Created by 斉藤 祐輔 on 2020/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBoilerplate/SwiftUIBoilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBoilerplateApp.swift
+//  SwiftUIBoilerplate
+//
+//  Created by 斉藤 祐輔 on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBoilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
